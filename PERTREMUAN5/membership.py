19cf4f8f7db9cc28d6c049b1c0d07c4d4915e767
Dapaaa8 <dadafakbar@gmail.com>
2024-08-06 09:40:45 +0700

    Add files via upload

---
+++ PERTREMUAN5/membership.py
@@ -0,0 +1,7 @@
+makanan = ["durian", "bakso", "ayam", "semangka"]
+
+print("apakah onana merupakan anggota", "onana" in makanan)
+print("apakah bakso merupakan anggota", "bakso" in makanan)
+
+print("apakah onana merupakan anggota", "onana" not in makanan)
+print("apakah bakso merupakan anggota", "bakso" not in makanan)
